55435e2877fcf1b74661943444de631d05ee7f58
AatroXiss <antoine.beaudesson@gmail.com>
2022-01-24 15:21:41 +0100

    v.0.0.7 THIS IS SHIT.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ __author__ = "Antoine 'AatroXiss' BEAUDESSON"
 __copyright__ = "Copyright 2021, Antoine 'AatroXiss' BEAUDESSON"
 __credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
 __license__ = ""
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
 __email__ = "antoine.beaudesson@gmail.com"
 __status__ = "Development"
@@ -26,6 +26,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # local application imports
 
@@ -113,6 +114,7 @@ def clean_dataset(data_cleaner):
     data_copy['Title'] = data_copy['Title'].apply(
         lambda x: 'Misc' if title_names.loc[x] == True else x)  # noqa 119
 
+
 def convert_formats(data_cleaner):
 
     # datasets
@@ -181,7 +183,6 @@ def split_data(data_cleaner, Target, data_copy_x_calc, data_copy_x_bin, data_cop
     """
     # datasets
     data_copy = data_cleaner[0]
-    data_test = data_cleaner[1]
 
     # copy
     train_copy_x, test_copy_x, train_copy_y, test_copy_y = train_test_split(
@@ -245,27 +246,118 @@ def main():
     plt.ylabel('Family Size (#)')
 
     plt.subplot(234)
-    plt.hist(x = [data_copy[data_copy['Survived']==1]['Fare'], data_copy[data_copy['Survived']==0]['Fare']], stacked=True, color=['g','r'], label=['Survived','Dead'])
+    plt.hist(x=[data_copy[data_copy['Survived'] == 1]['Fare'], data_copy[data_copy['Survived'] == 0]['Fare']], stacked=True, color=['g', 'r'], label=['Survived', 'Dead'])
     plt.title('Fare Histogram by Survival')
     plt.xlabel('Fare ($)')
     plt.ylabel('# of Passengers')
     plt.legend()
 
     plt.subplot(235)
-    plt.hist(x = [data_copy[data_copy['Survived']==1]['Age'], data_copy[data_copy['Survived']==0]['Age']], stacked=True, color=['g','r'], label=['Survived','Dead'])
+    plt.hist(x=[data_copy[data_copy['Survived'] == 1]['Age'], data_copy[data_copy['Survived'] == 0]['Age']], stacked=True, color=['g', 'r'], label=['Survived', 'Dead'])
     plt.title('Age Histogram by Survival')
     plt.xlabel('Age (Years)')
     plt.ylabel('# of Passengers')
     plt.legend()
 
     plt.subplot(236)
-    plt.hist(x = [data_copy[data_copy['Survived']==1]['FamilySize'], data_copy[data_copy['Survived']==0]['FamilySize']], stacked=True, color=['g','r'], label=['Survived','Dead'])
+    plt.hist(x=[data_copy[data_copy['Survived'] == 1]['FamilySize'], data_copy[data_copy['Survived'] == 0]['FamilySize']], stacked=True, color=['g', 'r'], label=['Survived', 'Dead'])
     plt.title('Family Size Histogram by Survival')
     plt.xlabel('Family Size (#)')
     plt.ylabel('# of passengers')
     plt.legend()
 
+    # graph individual features by survival
+    fig, saxis = plt.subplots(2, 3, figsize=(16, 12))
+
+    sns.barplot(x='Embarked', y='Survived', data=data_copy, ax=saxis[0, 0])
+    sns.barplot(x='Pclass', y='Survived', order=[1, 2, 3], data=data_copy, ax=saxis[0, 1])
+    sns.barplot(x='IsAlone', y='Survived', order=[1, 0], data=data_copy, ax=saxis[0, 2])
+    sns.pointplot(x='FareBin', y='Survived', data=data_copy, ax=saxis[1, 0])
+    sns.pointplot(x='AgeBin', y='Survived', data=data_copy, ax=saxis[1, 1])
+    sns.pointplot(x='FamilySize', y='Survived', data=data_copy, ax=saxis[1, 2])
+
+
+    # graph distribution of qualitative data: Pclass,
+    # we know class mattered in survival, so let's graph it
+    fig, (axis1, axis2, axis3) = plt.subplots(1, 3, figsize=(14, 12))
+    sns.boxplot(x='Pclass', y='Fare', hue='Survived', data=data_copy, ax=axis1)
+    axis1.set_title('Pclass vs Fare Survival Comparison')
+
+    sns.violinplot(x='Pclass', y='Age', hue='Survived', data=data_copy, split=True, ax=axis2)
+    axis2.set_title('Pclass vs Age Survival Comparison')
+
+    sns.boxplot(x='Pclass', y='FamilySize', hue='Survived', data=data_copy, ax=axis3)
+    axis3.set_title('Pclass vs Family Size Survival Comparison')
+
+
+    # graph distribution of qualitative data: Sex
+    # we know sex mattered in survival, now let's compare sex and a 2nd feature
+    fig, qaxis = plt.subplots(1, 3, figsize=(14, 12))
+
+    sns.barplot(x="Sex", y='Survived', hue="Embarked", data=data_copy, ax=qaxis[0])
+    axis1.set_title('Sex vs Embarked Survival comparison')
+
+    sns.barplot(x='Sex', y='Survived', hue='Pclass', data=data_copy, ax = qaxis[1])
+    axis1.set_title('Sex vs Pclass Survival comparison')
+
+    sns.barplot(x='Sex', y='Survived', hue='IsAlone', data=data_copy, ax=qaxis[2])
+    axis1.set_title('Sex vs IsAlone Survival Comparison')
+
+    # more side-by-sidfe comparisons
+    fig, (maxis1, maxis2) = plt.subplots(1, 2, figsize=(14, 12))
+
+    # how does family size factor with sex & survival compare
+    sns.pointplot(x="FamilySize", y="Survived", hue="Sex", data=data_copy,
+                  palette={"male": "blue", "female": "pink"},
+                  markers=["*", "o"],
+                  linestyles=["-","--"], ax=maxis1)
+    sns.pointplot(x="Pclass", y="Survived", hue="Sex", data=data_copy,
+                palette={"male": "blue", "female": "pink"},
+                markers=["*", "o"], linestyles=["-", "--"], ax=maxis2)
+
+    # How does embark port factor with class, sex, and survival
+    e = sns.FacetGrid(data_copy, col='Embarked')
+    e.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', ci=95.0, palette='deep')
+    e.add_legend()
+
+
+    # plot distributions of age of passengers who survived or did not survive
+    a = sns.FacetGrid(data_copy, hue='Survived', aspect=4)
+    a.map(sns.kdeplot, 'Age', shade=True)
+    a.set(xlim=(0, data_copy['Age'].max()))
+    a.add_legend()
+
+    # histogram comparision of sexn, class, and age by survival
+    h = sns.FacetGrid(data_copy, row='Sex', col='Pclass', hue='Survived')
+    h.map(plt.hist, 'Age', alpha=.75)
+    h.add_legend()
+
+    # pair plots of entire dataset
+    pp = sns.pairplot(data_copy, hue='Survived', palette='deep', size=1.2, diag_kind='kde', diag_kws=dict(shade=True), plot_kws=dict(s=10))
+    pp.set(xticklabels=[])
+
+    correlation_heatmap(data_copy)
+
     plt.show()
 
+
+
+#correlation heatmap of dataset
+def correlation_heatmap(df):
+    _ , ax = plt.subplots(figsize =(14, 12))
+    colormap = sns.diverging_palette(220, 10, as_cmap = True)
+
+    _ = sns.heatmap(
+        df.corr(), 
+        cmap = colormap,
+        square=True, 
+        cbar_kws={'shrink':.9 }, 
+        ax=ax,
+        annot=True, 
+        linewidths=0.1,vmax=1.0, linecolor='white',
+        annot_kws={'fontsize':12 }
+    )
+
+    plt.title('Pearson Correlation of Features', y=1.05, size=15)
 
 main()
